ee4aff0b52f4e1682b3e981856e8889025e15503
rspopov <rs.popov@outlook.com>
2022-10-13 13:21:46 +1000

    fix: исправлены замечания после ревью 1

--- stats-server/src/main/java/ru/practicum/ewm/EwmStatsServer.java
+++ stats-server/src/main/java/ru/practicum/ewm/EwmStatsServer.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EwmStatsServer {
     public static final String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
     public static void main(String[] args) {
         SpringApplication.run(EwmStatsServer.class, args);
     }
